7857d43e3930af6897ec16c6059935e7d0467e3e
ShubhamEng99 <shubham.eng1999@gmail.com>
2021-12-08 15:57:48 +0530

    added posts and comments

---
+++ controllers/posts_controller.js
@@ -0,0 +1,25 @@
+const Post = require('../models/post')
+const comment=require('../models/comment')
+module.exports.create = function(req, res){
+    Post.create({
+        content: req.body.content,
+        user: req.user._id
+    }, function(err, post){
+        if(err){console.log('error in creating a post'); return;}
+
+        return res.redirect('back');
+    });
+}
+
+module.exports.destroy=function(req,res){
+    Post.findById(req.params.id,function(err,post){
+        if(post.user==req.user.id){
+            post.remove();
+            comment.deleteMany({post:req.params.id},function(err){
+                return res.redirect('back')
+            })
+        }else{
+            return res.redirect('back');
+        }
+    })
+}
